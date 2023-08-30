949c6377df64c26d697818b1abaa178aa0ac856b
NageswararaoKN <131608963+NageswararaoKN@users.noreply.github.com>
2023-08-30 08:22:53 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
+import streamlit as st
+
 def largest(n1,n2,n3):
     mx = (n1 if (n1 > n2 and n1 > n3) else
           (n2 if (n2 > n1 and n2 > n3) else n3))
     return mx
 
-num1=int(input())
-num2=int(input())
-num3=int(input())
+def main():
+    st.title('Find the largest number ')
+    num1=st.number_input("Enter first number ")
+    num2=st.number_input("Enter second number ")
+    num3=st.number_input("Enter third number ")
 
-print("Largest number among " + str(num1) + ", " + str(num2) + " and " + str(num3) + " is " + str(largest(num1,num2,num3)))
+    if st.button("Find Largest"):
+        st.write("Largest number among " + str(num1) + ", " + str(num2) + " and " + str(num3) + " is " + str(largest(num1,num2,num3)))
+
+if __name__ == '__main__':
+    main()
